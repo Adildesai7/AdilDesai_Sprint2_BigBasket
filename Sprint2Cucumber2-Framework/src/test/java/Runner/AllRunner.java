cfbbd2ee3efff8b5dd3d21a9fa02693daf77d29f
Akil <Akil@LAPTOP-I8D1UIO6>
2021-10-13 12:57:24 +0530

    First Commit

--- Sprint2Cucumber2-Framework/src/test/java/Runner/AllRunner.java
+++ Sprint2Cucumber2-Framework/src/test/java/Runner/AllRunner.java
@@ -3,12 +3,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = "./src/test/java/feature/login.feature",
+		features = "./src/test/java/feature",
 		glue = "StepDef",
 		plugin = {"pretty","html:Reports/HTMLReport.html","json:target/Jsonreport.json",
 				"junit:target/JUnitreport.junit"}
 		)
 
-public class LoginRunner extends AbstractTestNGCucumberTests{
+public class AllRunner extends AbstractTestNGCucumberTests{
 
 }
